dc43726671a195c871ee8c771f3acbf528f5c478
Caitlin Hennessy <caitlin.hennessy@microsoft.com>
2017-11-30 17:02:20 -0800

    Make volume range match EV3's actual volume range

--- libs/music/shims.d.ts
+++ libs/music/shims.d.ts
@@ -3,15 +3,15 @@ declare namespace music {
 
     /**
      * Set the output volume of the sound synthesizer.
-     * @param volume the volume 0...256, eg: 128
+     * @param volume the volume 0...100, eg: 50
      */
     //% weight=96
     //% blockId=synth_set_volume block="set volume %volume"
     //% parts="speaker" blockGap=8
-    //% volume.min=0 volume.max=256
+    //% volume.min=0 volume.max=100
     //% help=music/set-volume
-    //% weight=1 shim=music::setVolume
-    function setVolume(volume: int32): void;
+    //% weight=1 volume.defl=50 shim=music::setVolume
+    function setVolume(volume?: int32): void;
 
     /**
      * Play a tone through the speaker for some amount of time.
